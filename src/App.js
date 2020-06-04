181d21bab244f072361d9e05aac77974b2661580
ishvar99 <ishanvarshney99@gmail.com>
2020-06-05 01:20:20 +0530

    styling done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React, { useState } from 'react';
-import './App';
+import './App.css';
 import fetchWeather from './API/fetchWeather';
 const App = () => {
   const [query, setQuery] = useState('');
-  // const [weather,setWeather]=useState();
+  const [weather, setWeather] = useState({});
   const search = async (e) => {
     if (e.key === 'Enter') {
       const data = await fetchWeather(query);
-      console.log(data);
+      setWeather(data);
+      setQuery('');
     }
   };
   return (
     <div className='main-container'>
       <input
         type='text'
+        className='search'
         onChange={(e) => setQuery(e.target.value)}
         placeholder='search...'
+        value={query}
         onKeyPress={search}
       />
+      {weather.main && (
+        <div className='city'>
+          <h2 className='city-name'>
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className='city-temp'>
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className='info' />
+          <img
+            className='city-icon'
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>{weather.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 };
