7ba5ea22ff8acbe1a9ddaaa82428a98fcfaf5551
ishvar99 <ishanvarshney99@gmail.com>
2020-06-05 03:23:51 +0530

    added offline file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -12,7 +12,25 @@ self.addEventListener('install', (event) => {
 });
 
 // Listen for requests
-self.addEventListener('fetch', (event) => {});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match('offline.html'));
+    })
+  );
+});
 
 // Activate service worker
-self.addEventListener('activate', (event) => {});
+self.addEventListener('activate', (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      cacheNames.map((cacheName) => {
+        if (!cacheWhiteList.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      });
+    })
+  );
+});
